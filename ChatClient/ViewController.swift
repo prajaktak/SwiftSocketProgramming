fc612df4852131cba03a21425a1b236f5e871fe2
Prajakta Kulkarni <prajakta.kulkarni@mirumagency.com>
2016-10-21 13:25:10 +0530

    basic infra for sockets is done

--- ChatClient/ViewController.swift
+++ ChatClient/ViewController.swift
@@ -8,16 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,StreamDelegate{
+    var inputStream:InputStream!
+    var outputStream:OutputStream!
+    
+    @IBOutlet weak var inputNameField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.initNetworkCommunication()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+//    @IBAction func joinChat(_ sender: UIButton) {
+//        let response:NSString = NSString.localizedStringWithFormat("i am :%@", inputNameField.text!)
+//        let adata :Data = response.data(using: String.Encoding.ascii.rawValue)!
+//        outputStream.write(adata., maxLength: adata.length)
+//        
+//    }
+//    - (IBAction)joinChat:(id)sender {
+//    
+//    NSString *response  = [NSString stringWithFormat:@"iam:%@", inputNameField.text];
+//    NSData *data = [[NSData alloc] initWithData:[response dataUsingEncoding:NSASCIIStringEncoding]];
+//    [outputStream write:[data bytes] maxLength:[data length]];
+//    
+//    }
+    
+    func initNetworkCommunication()
+    {
+        var readStream:  Unmanaged<CFReadStream>?
+        var writeStream: Unmanaged<CFWriteStream>?
+        
+        CFStreamCreatePairWithSocketToHost(nil, "localhost" as CFString, 80, &readStream, &writeStream)
+        inputStream = readStream!.takeRetainedValue()
+        outputStream = writeStream!.takeRetainedValue()
+        inputStream.delegate = self
+        outputStream.delegate =  self
+        
+        
+        var currentRunLoop: RunLoop?
+        
+        inputStream.schedule(in: currentRunLoop!, forMode:RunLoopMode.defaultRunLoopMode)
+        
+        inputStream.open()
+        outputStream.open()
+    }
+    
+    @IBAction func joinChat(_ sender: UIButton) {
     }
 
 
